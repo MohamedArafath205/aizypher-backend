f18345a94b47e58030649d41c93410fdf7461212
MohamedArafath205 <mohamedarafath205@gmail.com>
2024-09-10 21:56:08 +0530

    Fix CORS headers in initiate_payment.js and send-mail.js

--- api/initiate_payment.js
+++ api/initiate_payment.js
@@ -3,6 +3,16 @@ var util = require('./util.js');
 var sha512 = require('js-sha512');
 
 let initiate_payment = function (data, config, res) {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
 
   function isFloat(amt) {
     var regexp = /^\d+\.\d{1,2}$/;
