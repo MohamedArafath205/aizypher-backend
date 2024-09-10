cf976c67de0e9bbc1e1dfe509fa783a820565fb1
MohamedArafath205 <mohamedarafath205@gmail.com>
2024-09-10 23:12:57 +0530

    Refactor API utility functions for improved code structure and readability

--- api/util.js
+++ api/util.js
@@ -1,63 +1,84 @@
-let curl_call = function (url, data, method = 'POST') {
+const sha512 = require("js-sha512");
+const request = require("request"); // Consider replacing with axios or node-fetch
 
-  var request = require('request');
-  var options = {
-    'method': method,
-    'url': url,
-    'headers': {
-      'Content-Type': 'application/x-www-form-urlencoded'
+// Function to make HTTP requests
+let curl_call = function (url, data, method = "POST") {
+  const options = {
+    method: method,
+    url: url,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
     },
-
     form: data,
   };
-  return new Promise(function (resolve, reject) {
-    request(options, function (error, response) {
-      if (response) {
-        var data = JSON.parse(response.body)
-        return resolve(data);
-      } else
+
+  return new Promise((resolve, reject) => {
+    request(options, (error, response) => {
+      if (error) {
         return reject(error);
-    })
-  })
-}
+      }
+      try {
+        const data = JSON.parse(response.body);
+        return resolve(data);
+      } catch (parseError) {
+        return reject(parseError);
+      }
+    });
+  });
+};
 
-
+// Function to validate email format
 let validate_email = function (mail) {
-  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
-    return true;
-  }
-  return true;
-}
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail);
+};
 
+// Function to validate phone number
 let validate_phone = function (number) {
-  if ((number.length === 10)) {
-    return false;
-  }
+  return number.length === 10;
+};
 
-  return true;
-}
+// Function to generate SHA-512 hash
 let generateHash = function (data, config) {
+  const hashstring = [
+    config.key,
+    data.txnid,
+    data.amount,
+    data.productinfo,
+    data.name,
+    data.email,
+    data.udf1 || "",
+    data.udf2 || "",
+    data.udf3 || "",
+    data.udf4 || "",
+    data.udf5 || "",
+    data.udf6 || "",
+    data.udf7 || "",
+    data.udf8 || "",
+    data.udf9 || "",
+    data.udf10 || "",
+    config.salt,
+  ].join("|");
 
-  var hashstring = config.key + "|" + data.txnid + "|" + data.amount + "|" + data.productinfo + "|" + data.name + "|" + data.email +
-    "|" + data.udf1 + "|" + data.udf2 + "|" + data.udf3 + "|" + data.udf4 + "|" + data.udf5 + "|" + data.udf6 + "|" + data.udf7 + "|" + data.udf8 + "|" + data.udf9 + "|" + data.udf10;
-  hashstring += "|" + config.salt;
   data.hash = sha512.sha512(hashstring);
-  return (data.hash);
-}
+  return data.hash;
+};
 
+// Function to validate if a number is a float
 let validate_float = function (number) {
-  return parseFloat(number) === number
-}
+  return Number(parseFloat(number)) === number;
+};
 
+// Function to get base URL based on environment
 function get_query_url(env) {
-  let url_link = '';
-  if (env == 'prod') {
-    url_link = "https://dashboard.easebuzz.in/";
+  switch (env) {
+    case "prod":
+      return "https://dashboard.easebuzz.in/";
+    case "test":
+      return "https://testpay.easebuzz.in/";
+    default:
+      return "https://testpay.easebuzz.in/";
   }
-
-  return url_link;
 }
-
 
 exports.validate_mail = validate_email;
 exports.validate_phone = validate_phone;
