de6e3f66b8051e6be5d0761b6e2d67866b9c5579
MohamedArafath205 <mohamedarafath205@gmail.com>
2024-09-11 17:04:08 +0530

    please fix this

--- api/main.js
+++ api/main.js
@@ -3,37 +3,25 @@ const sha512 = require("js-sha512");
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const app = express();
 
-// CORS configuration
-const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: [
-    "X-CSRF-Token",
-    "X-Requested-With",
-    "Accept",
-    "Accept-Version",
-    "Content-Length",
-    "Content-MD5",
-    "Content-Type",
-    "Date",
-    "X-Api-Version",
-  ],
-  credentials: true,
-};
+const cors = require("cors");
 
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // Handle preflight requests
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["*"],
+  })
+);
 
 // Middleware
 app.use(bodyParser.json()); // For parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 
-// Static files
-app.use("/static", express.static(path.join(__dirname, "assets")));
-app.use("/view", express.static(path.join(__dirname, "views")));
+// // Static files
+// app.use("/static", express.static(path.join(__dirname, "assets")));
+// app.use("/view", express.static(path.join(__dirname, "views")));
 
 // View engine setup
 app.engine("html", require("ejs").renderFile);
@@ -48,20 +36,20 @@ const config = {
 };
 
 // Utility function to check reverse hash
-function checkReverseHash(response) {
-  const hashstring = `${config.salt}|${response.status}|${response.udf10}|${response.udf9}|${response.udf8}|${response.udf7}|${response.udf6}|${response.udf5}|${response.udf4}|${response.udf3}|${response.udf2}|${response.udf1}|${response.email}|${response.firstname}|${response.productinfo}|${response.amount}|${response.txnid}|${response.key}`;
-  const hash_key = sha512.sha512(hashstring);
-  return hash_key === response.hash;
-}
+// function checkReverseHash(response) {
+//   const hashstring = `${config.salt}|${response.status}|${response.udf10}|${response.udf9}|${response.udf8}|${response.udf7}|${response.udf6}|${response.udf5}|${response.udf4}|${response.udf3}|${response.udf2}|${response.udf1}|${response.email}|${response.firstname}|${response.productinfo}|${response.amount}|${response.txnid}|${response.key}`;
+//   const hash_key = sha512.sha512(hashstring);
+//   return hash_key === response.hash;
+// }
 
 // Response route
-app.post("/response", (req, res) => {
-  if (checkReverseHash(req.body)) {
-    res.json(req.body);
-  } else {
-    res.status(400).send("Invalid hash value");
-  }
-});
+// app.post("/response", (req, res) => {
+//   if (checkReverseHash(req.body)) {
+//     res.json(req.body);
+//   } else {
+//     res.status(400).send("Invalid hash value");
+//   }
+// });
 
 // Initiate Payment API
 app.post("/api/initiate_payment", async (req, res) => {
@@ -87,48 +75,48 @@ app.post("/api/initiate_payment", async (req, res) => {
 });
 
 // Transaction API
-app.post("/transaction", (req, res) => {
-  const data = req.body;
-  try {
-    const transaction = require("./transaction.js");
-    transaction.transaction(data, config, res);
-  } catch (error) {
-    res.status(500).send("Internal Server Error");
-  }
-});
+// app.post("/transaction", (req, res) => {
+//   const data = req.body;
+//   try {
+//     const transaction = require("./transaction.js");
+//     transaction.transaction(data, config, res);
+//   } catch (error) {
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 
-// Transaction Date API
-app.post("/transaction_date", (req, res) => {
-  const data = req.body;
-  try {
-    const transaction_date = require("./transaction_date.js");
-    transaction_date.transaction_date(data, config, res);
-  } catch (error) {
-    res.status(500).send("Internal Server Error");
-  }
-});
+// // Transaction Date API
+// app.post("/transaction_date", (req, res) => {
+//   const data = req.body;
+//   try {
+//     const transaction_date = require("./transaction_date.js");
+//     transaction_date.transaction_date(data, config, res);
+//   } catch (error) {
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 
-// Payout API
-app.post("/payout", (req, res) => {
-  const data = req.body;
-  try {
-    const payout = require("./payout.js");
-    payout.payout(data, config, res);
-  } catch (error) {
-    res.status(500).send("Internal Server Error");
-  }
-});
+// // Payout API
+// app.post("/payout", (req, res) => {
+//   const data = req.body;
+//   try {
+//     const payout = require("./payout.js");
+//     payout.payout(data, config, res);
+//   } catch (error) {
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 
-// Refund API
-app.post("/refund", (req, res) => {
-  const data = req.body;
-  try {
-    const refund = require("./refund.js");
-    refund.refund(data, config, res);
-  } catch (error) {
-    res.status(500).send("Internal Server Error");
-  }
-});
+// // Refund API
+// app.post("/refund", (req, res) => {
+//   const data = req.body;
+//   try {
+//     const refund = require("./refund.js");
+//     refund.refund(data, config, res);
+//   } catch (error) {
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 
 // Start the server
 const PORT = process.env.PORT || 3000;
