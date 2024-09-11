03271379d7157a1ff71267a806260922be7d4256
MohamedArafath205 <mohamedarafath205@gmail.com>
2024-09-11 17:54:37 +0530

    changed the code a little

--- api/main.js
+++ api/main.js
@@ -1,14 +1,15 @@
 require("dotenv").config();
-const sha512 = require("js-sha512");
 const express = require("express");
-const path = require("path");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const sha512 = require("js-sha512");
+
 const app = express();
 
-const cors = require("cors");
-
+// CORS configuration
 const corsOptions = {
-  origin: "http://localhost:3000", // Adjust according to your needs
+  origin: "http://localhost:3000", // Replace with your frontend URL
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
@@ -16,16 +17,9 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// Handle preflight requests
-app.options("*", cors());
-
 // Middleware
 app.use(bodyParser.json()); // For parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
-
-// // Static files
-// app.use("/static", express.static(path.join(__dirname, "assets")));
-// app.use("/view", express.static(path.join(__dirname, "views")));
 
 // View engine setup
 app.engine("html", require("ejs").renderFile);
@@ -39,36 +33,8 @@ const config = {
   enable_iframe: process.env.EASEBUZZ_IFRAME,
 };
 
-// Utility function to check reverse hash
-// function checkReverseHash(response) {
-//   const hashstring = `${config.salt}|${response.status}|${response.udf10}|${response.udf9}|${response.udf8}|${response.udf7}|${response.udf6}|${response.udf5}|${response.udf4}|${response.udf3}|${response.udf2}|${response.udf1}|${response.email}|${response.firstname}|${response.productinfo}|${response.amount}|${response.txnid}|${response.key}`;
-//   const hash_key = sha512.sha512(hashstring);
-//   return hash_key === response.hash;
-// }
-
-// Response route
-// app.post("/response", (req, res) => {
-//   if (checkReverseHash(req.body)) {
-//     res.json(req.body);
-//   } else {
-//     res.status(400).send("Invalid hash value");
-//   }
-// });
-
-// Initiate Payment API
+// Example route
 app.post("/api/initiate_payment", async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
   const data = req.body;
   try {
     const initiate_payment = require("./initiate_payment.js");
@@ -77,50 +43,6 @@ app.post("/api/initiate_payment", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
-
-// Transaction API
-// app.post("/transaction", (req, res) => {
-//   const data = req.body;
-//   try {
-//     const transaction = require("./transaction.js");
-//     transaction.transaction(data, config, res);
-//   } catch (error) {
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
-
-// // Transaction Date API
-// app.post("/transaction_date", (req, res) => {
-//   const data = req.body;
-//   try {
-//     const transaction_date = require("./transaction_date.js");
-//     transaction_date.transaction_date(data, config, res);
-//   } catch (error) {
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
-
-// // Payout API
-// app.post("/payout", (req, res) => {
-//   const data = req.body;
-//   try {
-//     const payout = require("./payout.js");
-//     payout.payout(data, config, res);
-//   } catch (error) {
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
-
-// // Refund API
-// app.post("/refund", (req, res) => {
-//   const data = req.body;
-//   try {
-//     const refund = require("./refund.js");
-//     refund.refund(data, config, res);
-//   } catch (error) {
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
 
 // Start the server
 const PORT = process.env.PORT || 3000;
